fe4f0df8ba746fbd362ff0b300ebf3205416d0b7
Utsav Virani <uvvirani004@gmail.com>
2021-03-07 10:38:42 +0530

    loginPage Designed and app icon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,26 +14,55 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
       ),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(
-                image: new AssetImage("lib/assets/logo_trans.png"),
-                fit: BoxFit.cover,
-                height: 300,
-              ),
-              Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(hintText: 'Mobile Number'),
-                  ),
-                ],
-              ),
-            ],
+        body: HomeView(),
+      ),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: new AssetImage("lib/assets/logo_trans.png"),
+            fit: BoxFit.cover,
+            height: 300,
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.only(left: 50, right: 50),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Mobile Number'),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                ),
+                // InternationalPhoneNumberInput(
+                //   // onInputChanged: ,
+                //   inputDecoration: InputDecoration(hintText: "Mobile NUmber"),
+                // ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: OutlineButton(
+                    onPressed: () {},
+                    child: Text("Send OTP"),
+                    hoverColor: Colors.blueAccent[400],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
